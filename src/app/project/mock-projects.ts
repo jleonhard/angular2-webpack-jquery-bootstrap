93ca4fe9148f80ca98b97c55e8e377492ecac58d
johannes.leonhard <johannes.leonhard@jvm.ch>
2017-09-06 11:35:42 +0200

    added mock-projects

---
+++ src/app/project/mock-projects.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by johannes.leonhard on 30.08.17.
+ */
+import { Project } from './project';
+
+export const PROJECTS: Project[] = [
+  { id: 1, name: 'Skoda Superb Kampagne' },
+  { id: 2, name: 'Skoda Fahrassistenzsysteme' },
+  { id: 3, name: 'Umweltarena Solarrechner' },
+  { id: 4, name: 'Samsung Hide&Seek' },
+  { id: 5, name: 'SLF Whiterisk' },
+  { id: 6, name: 'Raumgleiter App' }
+];
